19267db9df807d8a372443a3c318990ea7ba662c
hunteryavitz <hunteryavitz@protonmail.com>
2023-06-27 19:53:07 -0700

    initial commit

---
+++ src/main/java/com/hunteryavitz/blockchainapi/BlockchainApiApplication.java
@@ -0,0 +1,17 @@
+package com.hunteryavitz.blockchainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The BlockchainApiApplication class is the main class for the blockchain-api
+ * application. It is responsible for starting the Spring Boot application.
+ */
+@SpringBootApplication
+public class BlockchainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunteryavitz/blockchainapi/BlockchainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunteryavitz.blockchainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
