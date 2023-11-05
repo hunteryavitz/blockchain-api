edbb1804a1eae062451d28132ae3b5e6ffc37a2d
hunteryavitz <hunteryavitz@protonmail.com>
2023-11-05 15:22:02 -0700

    cleaned up health metric controller and unit tests

--- src/test/java/com/hunteryavitz/blockchainapi/controllers/HealthMetricControllerTests.java
+++ src/test/java/com/hunteryavitz/blockchainapi/controllers/HealthMetricControllerTests.java
@@ -7,8 +7,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 /**
- * Unit tests for the HealthMetric controller for the API.
+ * Unit tests for the HealthMetric controller.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {"spring.profiles.active=test"})
@@ -24,37 +28,106 @@ public class HealthMetricControllerTests {
     /**
      * The API version and endpoints.
      */
-    private static final String API_VERSION = "/api/v1";
+    private static final String API_VERSION = "/api/v1/healthMetric";
 
     /**
      * The health metric endpoint.
      */
-    private static final String UPDATE_PRODUCTION_ENDPOINT = "/healthMetric/updateProduction";
+    private static final String UPDATE_PRODUCTION_ENDPOINT = "/updateProductionHealth";
 
     /**
      * The health metric endpoint.
      */
-    private static final String GET_PRODUCTION_HEALTH_ENDPOINT = "/healthMetric/getProductionHealth";
+    private static final String GET_PRODUCTION_HEALTH_ENDPOINT = "/getProductionHealth";
 
     /**
-     * Tests the updateProduction endpoint.
+     * The health metric endpoint.
+     */
+    private static final String GET_EXCEPTION_HEALTH_ENDPOINT = "/getExceptionHealth";
+
+    /**
+     * The query parameter for testing.
+     */
+    private static final String QUERY_PARAM_TEST = "?test=true";
+
+    /**
+     * Tests the updateProductionHealth endpoint succeeds.
      */
     @Test
-    void testUpdateProduction() {
+    void testUpdateProductionHealth_onSuccess_returns200AndTrue() {
         ResponseEntity<Boolean> response = restTemplate.getForEntity(
-                API_VERSION + UPDATE_PRODUCTION_ENDPOINT, Boolean.class);
+                API_VERSION
+                        + UPDATE_PRODUCTION_ENDPOINT, Boolean.class);
+
         assert response.getStatusCode().is2xxSuccessful();
         assert (Boolean.TRUE.equals(response.getBody()));
     }
 
     /**
-     * Tests the getProductionHealth endpoint.
+     * Tests the updateProductionHealth endpoint fails.
      */
     @Test
-    void testGetProductionHealth() {
+    void testUpdateProductionHealth_onFail_returns200AndFalse() {
+        ResponseEntity<Boolean> response = restTemplate.getForEntity(
+                API_VERSION
+                        + UPDATE_PRODUCTION_ENDPOINT
+                        + QUERY_PARAM_TEST, Boolean.class);
+
+        assert response.getStatusCode().is2xxSuccessful();
+        assert (Boolean.FALSE.equals(response.getBody()));
+    }
+
+    /**
+     * Tests the getProductionHealth endpoint succeeds.
+     */
+    @Test
+    void testGetProductionHealth_onSuccess_returns200AndProductionHealth() {
         ResponseEntity<String> response = restTemplate.getForEntity(
-                API_VERSION + GET_PRODUCTION_HEALTH_ENDPOINT, String.class);
+                API_VERSION
+                        + GET_PRODUCTION_HEALTH_ENDPOINT, String.class);
+
         assert response.getStatusCode().is2xxSuccessful();
         assert (response.getBody() != null);
+    }
+
+    /**
+     * Tests the getProductionHealth endpoint fails.
+     */
+    @Test
+    void testGetProductionHealth_onFail_returns200AndNull() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                API_VERSION
+                        + GET_PRODUCTION_HEALTH_ENDPOINT
+                        + QUERY_PARAM_TEST, String.class);
+
+        assert response.getStatusCode().is2xxSuccessful();
+        assertNull (response.getBody());
+    }
+
+    /**
+     * Tests the getExceptionHealth endpoint succeeds.
+     */
+    @Test
+    void testGetExceptionHealth_onSuccess_returns200AndExceptionHealth() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                API_VERSION
+                        + GET_EXCEPTION_HEALTH_ENDPOINT, String.class);
+
+        assert response.getStatusCode().is2xxSuccessful();
+        assert (response.getBody() != null);
+    }
+
+    /**
+     * Tests the getExceptionHealth endpoint fails.
+     */
+    @Test
+    void testGetExceptionHealth_onFail_returns200AndNull() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                API_VERSION
+                        + GET_EXCEPTION_HEALTH_ENDPOINT
+                        + QUERY_PARAM_TEST, String.class);
+
+        assert response.getStatusCode().is2xxSuccessful();
+        assert (Objects.requireNonNull(response.getBody()).equals("[]"));
     }
 }
